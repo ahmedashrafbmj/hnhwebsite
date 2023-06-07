b0e5ddeb0ebfa388780b37f8167831b1d2d15868
Mian Muhammad Nadeem <LENOVO@ahmedinternew.hnh.local>
2023-06-07 19:16:28 +0500

    ahmed makedatya

--- src/config/baseurl.js
+++ src/config/baseurl.js
@@ -1,5 +1,5 @@
 const baseurl  = 
 {baseurl:"https://hnhwebsite.vercel.app/api",
-imageurl:"https://hnhwebsite.vercel.app/"
+imageurl:"https://hnhwebsite.vercel.app/uploads"
 }
 export default baseurl
